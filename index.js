ad9eae6f4bffe2c8b10958d7b2d2b76af70b768f
Rodolfo De Chiara <rodolfo.dechiara@tuta.io>
2022-05-18 17:23:34 +0200

    feat: add support for a whole game

--- index.js
+++ index.js
@@ -43,3 +43,47 @@ function playRound(playerSelection, computerSelection)
     
     return result;
 }
+
+function playGame()
+{
+    const rounds = 5;
+    let computerWins = 0;
+    let userWins = 0;
+    let ties = 0;
+
+    for(let i = 0; i < rounds; i++)
+    {
+            let userSelection = userPlay();
+            let computerSelection = computerPlay();
+
+            let result = playRound(userSelection, computerSelection);
+
+            console.log(result);
+
+            if(result === `You win! ${playerSelection} beats ${computerSelection}!`)
+            {
+                ++userWins;
+            }
+            else if(result === `You lose! ${computerSelection} beats ${playerSelection}!`)
+            {
+                ++computerWins;
+            }
+            else
+            {
+                ++ties;
+            }
+    }
+
+    if(userWins > computerWins)
+    {
+        console.log('You win!');
+    }
+    else if(computerWins > userWins)
+    {
+        console.log('Computer wins!');
+    }
+    else
+    {
+        console.log('It\'s a tie!');
+    }
+}
