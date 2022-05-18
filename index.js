886ee4456c6ee82eca7caa42150af4fd24484a35
Rodolfo De Chiara <rodolfo.dechiara@tuta.io>
2022-05-18 16:57:54 +0200

    feat: allow user to make a move

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const moves = ['Rock', 'Paper', 'Scissors'];
+const moves = ['rock', 'paper', 'scissors'];
 
 function randomIntNumber(start=0, end = 1)
 {
     return start + Math.floor(Math.random() * (end -start));
 }
+
 function computerPlay()
 {
     const cpu_choice = moves[randomIntNumber(0,2)];
     return cpu_choice;
+}
+
+function userPlay()
+{
+    let user_choice = undefined;
+    do
+    {
+        user_choice = prompt("Rock, Paper or Scissors?", 'rock').toLowerCase();
+    }
+    while(!moves.includes(user_choice));
+
+    return user_choice;
 }
