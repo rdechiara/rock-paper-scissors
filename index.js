fb5d7e652d11fe1680c7ec92c6ebc53804076815
Rodolfo De Chiara <rodolfo.dechiara@tuta.io>
2022-05-18 16:44:33 +0200

    ref: move valid moves to global scope

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const moves = ['Rock', 'Paper', 'Scissors'];
+
 function randomIntNumber(start=0, end = 1)
 {
     return start + Math.floor(Math.random() * (end -start));
 }
 function computerPlay()
 {
-    const moves = ['Rock', 'Paper', 'Scissors'];
     const cpu_choice = moves[randomIntNumber(0,2)];
     return cpu_choice;
 }
